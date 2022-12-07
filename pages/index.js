f53b9b75ed990d178520e86dc33afe6b6993ebb4
tayyabtahir870@gmail.com <tayyabtahir870@gmail.com>
2022-12-07 18:05:32 +0500

    getnum

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Shufflecard from '../Components/Elements/Shufflecard'
+
+
+function index() {
+  return (
+    <div>
+           <Shufflecard/>
+    </div>
+  )
+}
+
+export default index
